178c3f1f03c5a860a6b8b522b9d39cf593df7727
filippo <filippo.luppi@gmail.com>
2020-01-06 16:46:25 +0100

    DeviceList must be cleared correctly

--- bootstrap.rb
+++ bootstrap.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/ruby
+require 'ostruct'
 require "yaml"
 config=YAML.load_file('./config.yaml')
 #SOME INIT
 puts "----Network Traker----"
+puts "Secure Network this program will perform a MITM Attack without ssl script against every device in network using bettercap sudo bettercap -G GATEWAY --target DEVICE_IP --no-discovery  --sniffer"
+
 #TAKE SOME CONFIGURATION from YAML
 #GATEWAY IP
 gateway_ip=config['GATEWAY']
@@ -16,19 +19,19 @@ else
 end
 
 #EXCLUDED IP
-#excluded_devices=config['EXCLUDED_DEVICES']
+excluded_devices=config['EXCLUDED_DEVICES']
 
 #CHECK FORM REQUIRED SOFTWARE INSTALLATION
 #check for bettercap installation
 
-if(%x(gem list | grep 'bettercap' -c) < 1)
+if(%x(gem list | grep 'bettercap' -c).to_i < 1)
   puts"Error: Bettercap must be installed as gem"
   abort("Error: Bettercap must be installed as gem")
 else
 	puts "Bettercap finded! "
 end
 #check for arp-scan installation
-if(exec("./is_installed.sh arp-scan")==0)
+if(%x(./is_installed.sh arp-scan).to_i != 0)
   puts"Error: arp-scan must be installed as gem"
   abort("Error: arp-scan must be installed as gem")
 else
@@ -37,9 +40,28 @@ end
 #START
 puts"Let's start discovering your network..."
 #Discover devices in network
-#DevicesInNetwork=system("sudo arp-scan --localnet")
+cmd="sudo arp-scan --localnet |grep -E '\\b([0-9]{1,3}\\.){3}[0-9]{1,3}\\b'"
+value=%x[ #{cmd} ]
+puts "Founded devices:"
 
-puts "Secure Network this program will perform a MITM Attack without ssl script against every device in network using bettercap sudo bettercap -G GATEWAY --target DEVICE_IP --no-discovery  --sniffer"
-puts DevicesInNetwork
+Devices=Array.new
+value.each_line do |line|
+    #split each line to collet IP MAC NAME
+	device=OpenStruct.new 
+	device.ip=line.split(' ')[0]
+	device.mac=line.split(' ')[1]
+	device.name=line.split(' ')[2]
+	Devices<<device
+end
+
+puts "---- DEVICE LIST----"
+Devices.each{|a| puts a}
+
+puts "cleaning Device list..."
+Devices.delete_if{|device| excluded_devices.include? (device.ip)}
+
+puts "---- NEW DEVICES LIST----"
+Devices.each{|a| puts a}
+
 
 #fork{exec("ls")}
