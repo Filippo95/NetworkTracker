6c00612704a5b1018b03f71e5335d472cd7ca834
Filippo Luppi <filippoluppi@MacBook-Pro-di-Filippo.local>
2020-01-06 09:53:48 +0100

    second

--- bootstrap.rb
+++ bootstrap.rb
@@ -6,28 +6,32 @@ config=YAML.load_file('./config.yaml')
 #GATEWAY IP
 gateway_ip=config['GATEWAY']
 #gateway ip can be given as param
-if ARGV[0]!=""
-  gateway_ip=ARGV[0];
+if ARGV.empty?
+  puts"no arguments"
+else
+  if ARGV[0]!=""
+    gateway_ip=ARGV[0];
+  end
+end
 
 #EXCLUDED IP
 excluded_devices=config['EXCLUDED_DEVICES']
-
 
 #CHECK FORM REQUIRED SOFTWARE INSTALLATION
 #check for bettercap installation
 if(system("gem list | gerp 'bettercap' -c")<1)
   puts"Error: Bettercap must be installed as gem"
   abort("Error: Bettercap must be installed as gem")
-
+end
 #check for arp-scan installation
 if(system("./is_installed.sh arp-scan")==0)
   puts"Error: arp-scan must be installed as gem"
   abort("Error: arp-scan must be installed as gem")
-
+end
 #START
 #Discover devices in network
 DevicesInNetwork=system("sudo arp-scan --localnet")
 
 puts "Secure Network \n this program will perform a MITM Attack without ssl script against every device in network using bettercap
 sudo bettercap -G GATEWAY --target DEVICE_IP --no-discovery  --sniffer"
-puts DevicesInNetwork 
+puts DevicesInNetwork
